96f17a3efcb1e6913e1d7b091663f6882706f189
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-02-17 11:13:28 +0100

    Fixed svg attribute

--- app/c/[slug]/page.tsx
+++ app/c/[slug]/page.tsx
@@ -39,7 +39,7 @@ export default async function Home({ params }: { params: { slug: string } }) {
           <path
             fill="transparent"
             stroke="black"
-            stroke-width="10"
+            strokeWidth="10"
             d="M -10,50 L 90,50 M 50,10 L 90,50 L 50,90"
           ></path>
         </svg>{' '}
