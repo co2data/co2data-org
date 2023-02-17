f2db434c4c2fdbf2e95e5b6d800f48445e9b3de5
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-02-17 09:05:29 +0100

    Improved overview styling

--- components/co2-average.tsx
+++ components/co2-average.tsx
@@ -4,17 +4,26 @@ import Link from 'next/link'
 type Props = { co2Average: Co2Average }
 const Co2Average = ({ co2Average }: Props) => {
   return (
-    <li key={co2Average.slug} className="rounded border p-8">
-      <p>
+    <li
+      key={co2Average.slug}
+      className="rounded border-4 border-blue-700 bg-blue-100"
+    >
+      <p className="bg-blue-700 px-4 py-2 font-bold text-white">
         <Link href={`/c/${co2Average.slug}`}>{co2Average.title}</Link>
       </p>
-      <p>
+      <p className="px-4 py-2 text-xs ">
         Possible CO<sub>2</sub>e per person-year
       </p>
-      <p>{co2Average.avg_per_year} g</p>
-      <p>
-        1 {co2Average.unit} {co2Average.avg_per_unit} g CO<sub>2</sub>e
+      <p className="px-4 text-right text-3xl font-bold">
+        {+parseFloat(((co2Average.avg_per_year ?? 1) / 1000).toFixed(3))}{' '}
+        <span className="text-sm font-normal">kg</span>
       </p>
+      <div className="flex justify-between gap-2">
+        <p className="py-2 pl-4 text-xs">1 {co2Average.unit}</p>
+        <p className="py-2 pr-4 text-xs">
+          {(co2Average.avg_per_unit ?? 1) / 1000} kg CO<sub>2</sub>e
+        </p>
+      </div>
     </li>
   )
 }
