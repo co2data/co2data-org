81611452e9d3829e0fce87b9956612d6732448dd
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-02-24 12:21:45 +0100

    Nicer layout and tooltip

--- app/c/[slug]/page.tsx
+++ app/c/[slug]/page.tsx
@@ -17,8 +17,8 @@ export default async function Home({ params }: { params: { slug: string } }) {
   const sources = await getAllSourcesByCo2ProducerId(co2Average?.id)
 
   return (
-    <main className="">
-      <header className="space-y-8">
+    <main className="overflow-hidden">
+      <header className="space-y-4">
         <nav>
           <ol className="flex gap-3">
             <li>
@@ -27,34 +27,36 @@ export default async function Home({ params }: { params: { slug: string } }) {
             <li className="ml-2 list-['>'] pl-2">{co2Average.title}</li>
           </ol>
         </nav>
-        <h1 className="text-4xl font-bold leading-6">
-          {co2Average.title}
-          <br />
-          <small className="text-sm font-normal">
-            CO<sub>2</sub> emissions
-          </small>
-        </h1>
+        <div className="space-y-4 rounded-lg border border-sky-600 bg-sky-50 p-8">
+          <h1 className="text-4xl font-bold leading-6">
+            {co2Average.title}
+            <br />
+            <small className="text-sm font-normal">
+              CO<sub>2</sub> emissions
+            </small>
+          </h1>
+          <p className="text-4xl">
+            <span>1 {co2Average.unit}</span>
+            <svg className="inline-block h-6 w-12" viewBox="-40 0 140 120">
+              <path
+                fill="transparent"
+                stroke="black"
+                strokeWidth="10"
+                d="M -10,50 L 90,50 M 50,10 L 90,50 L 50,90"
+              ></path>
+            </svg>
+            <span className="font-bold">
+              {+parseFloat(((co2Average.avg_per_unit ?? 1) / 1000).toFixed(3))}
+            </span>
+            &nbsp;
+            <span className="text-base">
+              kg&nbsp;CO<sub>2</sub>e
+            </span>
+          </p>
+        </div>
       </header>
-      <section className="mt-16 space-y-8">
-        <p className="text-4xl">
-          <span>1 {co2Average.unit}</span>
-          <svg className="inline-block h-6 w-12" viewBox="-40 0 140 120">
-            <path
-              fill="transparent"
-              stroke="black"
-              strokeWidth="10"
-              d="M -10,50 L 90,50 M 50,10 L 90,50 L 50,90"
-            ></path>
-          </svg>{' '}
-          <span className="font-bold">
-            {+parseFloat(((co2Average.avg_per_unit ?? 1) / 1000).toFixed(3))}
-          </span>{' '}
-          <span className="text-base">
-            kg CO<sub>2</sub>e
-          </span>
-        </p>
-      </section>
-      <div className="mt-32 gap-32 lg:flex">
+
+      <div className="mt-8 space-y-16">
         <section>
           <PersonalCo2 co2Average={co2Average} />
         </section>
--- components/personal-co2.tsx
+++ components/personal-co2.tsx
@@ -34,116 +34,142 @@ export default function PersonalCo2({
   console.log('render', factorOfPersonYearFootprint, parts)
 
   return (
-    <div className="max-w-sm overflow-hidden rounded-lg border border-sky-600 bg-sky-50 p-8">
-      <h2 className="text-lg font-bold">
-        Personal CO<sub>2</sub> footprint
-      </h2>
-      <div className="">
-        <div className="flex justify-around px-12">
-          <svg className="absolute h-0 w-0">
-            <defs>
-              <mask id="segmentMask">
-                <polygon
-                  points="150,-150 350,-150 350,350 150,350"
-                  fill="white"
-                  transform={'rotate(0 150,100)'}
-                />
-                <polygon
-                  points="150,-150 350,-150 350,350 150,350"
-                  fill={isLeft ? 'white' : 'black'}
-                  transform={`rotate(${angleMask} 150,100)`}
-                />
-              </mask>
-            </defs>
-          </svg>
-          {parts.map((state) => (
-            <PieChart key={Math.random()} state={state} />
-          ))}
-        </div>
-        <div className="tooltip-container text-center">
-          <b>{Math.round(co2PerYear / 60000)} %</b>
-          <br />
-          of the <b>total</b> CO<sub>2</sub>e Footprint of 1{' '}
-          <svg
-            viewBox="0 -2 20 55"
-            className="inline-block h-6 w-2 -translate-y-1"
-          >
-            <circle cx="9" cy="9" r="4" />
-            <path d="M9,32l0,19l4,0l0,-30l1,0l0,13l3,0l0,-18l-18,0l0,18l3,0l0,-13l1,0l-0,30l4,0l0,-19" />
-          </svg>
-          &ensp;
-          <a href="#" role="button" aria-label="More information">
-            &#9432;
-          </a>
-          {/* {totalFootprintInformationTooltip} */}
-        </div>
-        <div className="mt-8">
-          <div className="flex items-baseline justify-between">
-            <p>
-              CO<sub>2</sub>e per consumption
-            </p>
-            <p className="text-2xl">
-              <b>{+parseFloat((co2PerConsumption / 1000).toFixed(1))}</b>
-              &nbsp;kg
-            </p>
+    <div className="rounded-lg border border-sky-600 bg-sky-50 p-8">
+      <div className="flex flex-col justify-between gap-x-24 gap-y-8 lg:flex-row">
+        <div className="flex-1">
+          <h2 className="text-lg font-bold">
+            Personal CO<sub>2</sub> footprint
+          </h2>
+          <div className="flex justify-around px-12">
+            <svg className="absolute h-0 w-0">
+              <defs>
+                <mask id="segmentMask">
+                  <polygon
+                    points="150,-150 350,-150 350,350 150,350"
+                    fill="white"
+                    transform={'rotate(0 150,100)'}
+                  />
+                  <polygon
+                    points="150,-150 350,-150 350,350 150,350"
+                    fill={isLeft ? 'white' : 'black'}
+                    transform={`rotate(${angleMask} 150,100)`}
+                  />
+                </mask>
+              </defs>
+            </svg>
+            {parts.map((state) => (
+              <PieChart key={Math.random()} state={state} />
+            ))}
           </div>
-          <div className="flex items-baseline justify-between">
-            <p>
-              CO
-              <sub>2</sub>e per year
-            </p>
-            <p className="text-2xl">
-              <b>{+parseFloat((co2PerYear / 1000).toFixed(1))}</b>&nbsp;kg
-            </p>
+          <div className="tooltip-container text-center">
+            <b>{Math.round(co2PerYear / 60000)} %</b>
+            <br />
+            of the <b>total</b> CO<sub>2</sub>e Footprint of a{' '}
+            <svg
+              viewBox="0 -2 20 55"
+              className="inline-block h-6 w-2 -translate-y-1"
+            >
+              <circle cx="9" cy="9" r="4" />
+              <path d="M9,32l0,19l4,0l0,-30l1,0l0,13l3,0l0,-18l-18,0l0,18l3,0l0,-13l1,0l-0,30l4,0l0,-19" />
+            </svg>
+            .
+            <span
+              role="button"
+              aria-label="More information"
+              className="rounded text-sky-600 hover:bg-sky-100"
+            >
+              &#9432;
+            </span>
+            <div className="tooltip text-sky-600">
+              <p>
+                The average{' '}
+                <svg
+                  viewBox="0 -2 20 55"
+                  className="inline-block h-6 w-2 -translate-y-1"
+                >
+                  <circle cx="9" cy="9" r="4" />
+                  <path d="M9,32l0,19l4,0l0,-30l1,0l0,13l3,0l0,-18l-18,0l0,18l3,0l0,-13l1,0l-0,30l4,0l0,-19" />
+                </svg>{' '}
+                has a <b>total</b>* CO<sub>2</sub>e footprint of 6000t per year.
+              </p>
+              <p>
+                <small>
+                  * Not only this contributor, but all CO<sub>2</sub>e combined
+                  of one person in one year.
+                </small>
+              </p>
+            </div>
           </div>
         </div>
-        <div className="mt-8 block touch-pan-y">
-          <label htmlFor="amountInput">
-            <div className="flex justify-between">
-              <div>Single consmuption</div>
-              <div>
-                <b>{consumption}</b>&ensp;
-                {co2Average.unit}
-              </div>
+        <div className="flex-[2]">
+          <div className="ml-auto mr-auto max-w-xs lg:mr-0">
+            <div className="flex items-baseline justify-between">
+              <p>
+                CO<sub>2</sub>e per consumption
+              </p>
+              <p className="text-2xl">
+                <b>{+parseFloat((co2PerConsumption / 1000).toFixed(1))}</b>
+                &nbsp;kg
+              </p>
             </div>
-          </label>
-          <input
-            id="amountInput"
-            type="range"
-            className="range touch-pan-x text-sky-600"
-            min={co2Average.single_consumption_from}
-            max={co2Average.single_consumption_to}
-            step={
-              (co2Average.single_consumption_to -
-                co2Average.single_consumption_from) /
-              50
-            }
-            value={consumption}
-            onChange={(e) => setConsumption(Number(e.target.value))}
-          />
-          <label htmlFor="timesPerYearInput" className="mt-4 block">
-            <div className="flex justify-between">
-              <div>Times per year</div>
-              <div>
-                ×&nbsp;<b>{Math.round(times)}</b>
-              </div>
+            <div className="flex items-baseline justify-between">
+              <p>
+                CO
+                <sub>2</sub>e per year
+              </p>
+              <p className="text-2xl">
+                <b>{+parseFloat((co2PerYear / 1000).toFixed(1))}</b>&nbsp;kg
+              </p>
             </div>
-          </label>
-
-          <input
-            id="timesPerYearInput"
-            type="range"
-            className="range touch-pan-x text-sky-600"
-            min={co2Average.times_per_year_from}
-            max={co2Average.times_per_year_to}
-            step={
-              (co2Average.times_per_year_to - co2Average.times_per_year_from) /
-              50
-            }
-            value={times}
-            // onInput={(e) => setTimes(e)}
-            onChange={(e) => setTimes(Number(e.target.value))}
-          />
+          </div>
+          <div className="mt-8 block touch-pan-y">
+            <label htmlFor="amountInput">
+              <div className="flex justify-between">
+                <div>Single consumption</div>
+                <div>
+                  <b>{consumption}</b>&ensp;
+                  {co2Average.unit}
+                </div>
+              </div>
+            </label>
+            <input
+              id="amountInput"
+              type="range"
+              className="range touch-pan-x text-sky-600"
+              min={co2Average.single_consumption_from}
+              max={co2Average.single_consumption_to}
+              step={
+                (co2Average.single_consumption_to -
+                  co2Average.single_consumption_from) /
+                50
+              }
+              value={consumption}
+              onChange={(e) => setConsumption(Number(e.target.value))}
+            />
+            <label htmlFor="timesPerYearInput" className="mt-4 block">
+              <div className="flex justify-between">
+                <div>Times per year</div>
+                <div>
+                  ×&nbsp;<b>{Math.round(times)}</b>
+                </div>
+              </div>
+            </label>
+            <input
+              id="timesPerYearInput"
+              type="range"
+              className="range touch-pan-x text-sky-600"
+              min={co2Average.times_per_year_from}
+              max={co2Average.times_per_year_to}
+              step={
+                (co2Average.times_per_year_to -
+                  co2Average.times_per_year_from) /
+                50
+              }
+              value={times}
+              // onInput={(e) => setTimes(e)}
+              onChange={(e) => setTimes(Number(e.target.value))}
+            />
+          </div>
         </div>
       </div>
     </div>
