6f175635dd00a5e7890e7a861cb7ebf436b86562
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-03-11 13:58:15 +0100

    Removed mysql2 from db loader

--- infra/db/index.ts
+++ infra/db/index.ts
@@ -1,21 +1,9 @@
-import { createPool } from 'mysql2'
-import { Kysely, MysqlDialect } from 'kysely'
+import { Kysely } from 'kysely'
 import { DB } from './types'
 import { PlanetScaleDialect } from 'kysely-planetscale'
 declare global {
   var EdgeRuntime: string | undefined
 }
-const dialect =
-  process.env.NODE_ENV !== 'development' ||
-  typeof globalThis.EdgeRuntime === 'string'
-    ? new PlanetScaleDialect({ url: process.env.DATABASE_URL })
-    : new MysqlDialect({
-        pool: createPool({
-          host: '127.0.0.1',
-          user: 'root',
-          password: 'password',
-          database: 'co2data-org',
-        }),
-      })
+const dialect = new PlanetScaleDialect({ url: process.env.DATABASE_URL })
 
 export const db = new Kysely<DB>({ dialect })
