f878f3a0c331f5f382407d1fbc8704a0b228b6a3
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-03-11 13:49:49 +0100

    Check for edge when loading db

--- infra/db/index.ts
+++ infra/db/index.ts
@@ -2,10 +2,14 @@ import { createPool } from 'mysql2'
 import { Kysely, MysqlDialect } from 'kysely'
 import { DB } from './types'
 import { PlanetScaleDialect } from 'kysely-planetscale'
-
+declare global {
+  var EdgeRuntime: string | undefined
+}
 const dialect =
-  process.env.NODE_ENV === 'development'
-    ? new MysqlDialect({
+  process.env.NODE_ENV !== 'development' ||
+  typeof globalThis.EdgeRuntime === 'string'
+    ? new PlanetScaleDialect({ url: process.env.DATABASE_URL })
+    : new MysqlDialect({
         pool: createPool({
           host: '127.0.0.1',
           user: 'root',
@@ -13,6 +17,5 @@ const dialect =
           database: 'co2data-org',
         }),
       })
-    : new PlanetScaleDialect({ url: process.env.DATABASE_URL })
 
 export const db = new Kysely<DB>({ dialect })
