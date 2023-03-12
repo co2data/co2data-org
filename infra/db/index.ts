fee658a7e8d0996f3d485fbc14d2fd8adc42757b
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-03-12 07:54:18 +0100

    Test keeping mysql for local development

--- infra/db/index.ts
+++ infra/db/index.ts
@@ -1,9 +1,19 @@
-import { Kysely } from 'kysely'
+import { createPool } from 'mysql2'
+import { Kysely, MysqlDialect } from 'kysely'
 import { DB } from './types'
 import { PlanetScaleDialect } from 'kysely-planetscale'
 declare global {
   var EdgeRuntime: string | undefined
 }
-const dialect = new PlanetScaleDialect({ url: process.env.DATABASE_URL })
+const dialect = true
+  ? new PlanetScaleDialect({ url: process.env.DATABASE_URL })
+  : new MysqlDialect({
+      pool: createPool({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'password',
+        database: 'co2data-org',
+      }),
+    })
 
 export const db = new Kysely<DB>({ dialect })
