c44ff8296d88568de9bd413336ff62ec3e3e71c2
Philip Schönholzer <phi.sch@hotmail.ch>
2023-03-17 12:09:16 +0100

    Use and import mysql driver on on local dev env only (#11)
    closes #1

--- infra/db/index.ts
+++ infra/db/index.ts
@@ -1,10 +1,31 @@
-import { createPool } from 'mysql2'
 import { Kysely, MysqlDialect } from 'kysely'
 import { DB } from './types'
 import { PlanetScaleDialect } from 'kysely-planetscale'
-declare global {
-  var EdgeRuntime: string | undefined
-}
-const dialect = new PlanetScaleDialect({ url: process.env.DATABASE_URL })
 
-export const db = new Kysely<DB>({ dialect })
+const dialect = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development': {
+      console.log(
+        `Loading MySQL driver for local development environment @${
+          process.env.DATABASE_URL?.split('@')[1]
+        }`
+      )
+
+      const { createPool } = require('mysql2')
+      return new MysqlDialect({
+        pool: createPool({
+          host: '127.0.0.1',
+          user: 'root',
+          password: 'password',
+          database: 'co2data-org',
+        }),
+      })
+    }
+
+    default: {
+      return new PlanetScaleDialect({ url: process.env.DATABASE_URL })
+    }
+  }
+}
+
+export const db = new Kysely<DB>({ dialect: dialect() })
