1f0b94ed5a76b1770fe321e7249c414b2ee72263
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-06-30 18:24:13 +0200

    Fixed drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,5 +2,7 @@ import type { Config } from 'drizzle-kit'
 
 export default {
   schema: './infra/db/schema.ts',
-  connectionString: 'mysql://root:password@127.0.0.1:3306/co2data-org',
+  dbCredentials: {
+    connectionString: 'mysql://root:password@127.0.0.1:3306/co2data-org',
+  },
 } satisfies Config
