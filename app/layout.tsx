db72368a8447aa16ec40041583ac7084d1d58efc
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-06-30 13:56:55 +0200

    Removed not needed head fragment

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
       <body className="flex min-h-screen flex-col">
         <Header />
         <div className="container mx-auto flex-1 px-4 py-16">{children}</div>
