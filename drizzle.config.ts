5b2559497e82d8386e8946ed8ae9de41f084aec9
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-07-26 15:27:33 +0200

    Updated deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,4 +5,7 @@ export default {
   dbCredentials: {
     connectionString: 'mysql://root:password@127.0.0.1:3306/co2data-org',
   },
+  driver: 'mysql2',
+  out: './infra/db/',
+
 } satisfies Config
