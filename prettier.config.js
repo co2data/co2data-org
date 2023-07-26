d1d52d79e82b592d80d3f1e1aa02e1345d2eac81
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-07-26 16:05:28 +0200

    Fixed prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,5 @@ module.exports = {
   trailingComma: "es5",
   semi: false,
   singleQuote: true,
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
 }
