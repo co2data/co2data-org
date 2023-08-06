e747957d5858a64823f09f87b9172b6e062ac764
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-08-06 17:34:11 +0200

    Use language (node, pnpm) and services (mysql) in devenv

--- devenv.nix
+++ devenv.nix
@@ -4,13 +4,23 @@
 
   # https://devenv.sh/packages/
   packages = [ 
-    pkgs.docker
-    pkgs.nodejs-18_x
-    pkgs.nodePackages.pnpm
     pkgs.pscale 
   ];
 
-  dotenv.enable = true;
+  # Devenv is not able to parse comments in .env files https://github.com/cachix/devenv/issues/80
+  dotenv.disableHint = true;
+
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_18;
+    corepack.enable = true;
+  };
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql80;
+    initialDatabases = [{ name = "co2data-org"; }];
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
