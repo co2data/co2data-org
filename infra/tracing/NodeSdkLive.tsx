b7043899a2400472ee0a7e715faae44f59149203
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-11-18 12:17:05 +0100

    Use config for env vars for grafana tempo

--- infra/tracing/NodeSdkLive.tsx
+++ infra/tracing/NodeSdkLive.tsx
@@ -1,17 +1,33 @@
 import { NodeSdk } from '@effect/opentelemetry'
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { Config, ConfigSecret, Duration, Effect, Layer } from 'effect'
 
-export const NodeSdkLive = NodeSdk.layer(() => ({
-  resource: {
-    serviceName: 'nextjs',
-  },
-  spanProcessor: new BatchSpanProcessor(
-    new OTLPTraceExporter({
-      url: process.env.OTLP_URL,
+const GrafanaTempoConfig = Config.nested('OTLP')(
+  Config.all({
+    url: Config.string('URL'),
+    auth: Config.secret('AUTH'),
+  })
+)
+
+export const NodeSdkLive = Layer.unwrapEffect(
+  Effect.gen(function* ($) {
+    const { url, auth } = yield* $(Effect.config(GrafanaTempoConfig))
+
+    const traceExporter = new OTLPTraceExporter({
+      url,
       headers: {
-        Authorization: process.env.OTLP_AUTH,
+        Authorization: ConfigSecret.value(auth),
       },
     })
-  ),
-}))
+
+    return NodeSdk.layer(() => ({
+      resource: {
+        serviceName: 'nextjs',
+      },
+      spanProcessor: new BatchSpanProcessor(traceExporter, {
+        scheduledDelayMillis: Duration.toMillis('1 seconds'),
+      }),
+    }))
+  })
+)
