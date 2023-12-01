1311ec016dae2e0017c58038266f168ab5b7cd92
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-12-01 14:27:02 +0100

    Fixed build

--- app/og/[slug]/route.tsx
+++ app/og/[slug]/route.tsx
@@ -1,6 +1,12 @@
-import { run } from '@/adapter/effect'
+import { co2RepoLive } from '@/domain/co2'
+import { Effect, flow } from 'effect'
 import { createOgImageResponse } from './create-og-image-response'
 
 export const runtime = 'edge'
 
-export const GET = run(createOgImageResponse)
+export const GET = flow(
+  // can't use run because of edge (OTel)
+  createOgImageResponse,
+  Effect.provide(co2RepoLive),
+  Effect.runPromise
+)
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,8 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './infra/db/schema.ts',
   dbCredentials: {
-    connectionString: 'mysql://root:password@127.0.0.1:3306/co2data-org',
+    uri: 'mysql://root:password@127.0.0.1:3306/co2data-org',
   },
   driver: 'mysql2',
   out: './infra/db/',
-
 } satisfies Config
