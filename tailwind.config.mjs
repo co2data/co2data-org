883ea1595ba0710270ce421789ae083ab671729e
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2023-12-02 11:23:30 +0100

    Revert "Revert "Switch to ESM""
    This reverts commit f03702c50902ec163ad7032cc902c9dd1df1dd1a.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -73,12 +73,12 @@ module.exports = {
       },
       keyframes: {
         'accordion-down': {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
+          to: { height: '0' },
         },
       },
       animation: {
@@ -89,3 +89,5 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
 }
+
+export default config
