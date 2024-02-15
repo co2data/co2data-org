2085e3b64c943e8cc745742a2131ffe13b51aa7a
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-02-15 19:06:04 +0100

    Added biome

--- prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Options} */
-const config = {
-  trailingComma: 'es5',
-  semi: false,
-  singleQuote: true,
-  plugins: ['prettier-plugin-tailwindcss'],
-}
-
-export default config
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-ignore
   plugins: [react()],
   test: {
     setupFiles: ['./setup.ts'],
