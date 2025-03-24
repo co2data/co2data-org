26164af241c61e43bc1354f696f22c3fa26d328f
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-03-24 13:59:24 +0100

    Clean up logs

--- adapter/db/orm-client.ts
+++ adapter/db/orm-client.ts
@@ -8,7 +8,6 @@ import * as schema from './schema'
 
 const makeOrmClient = () => {
   const url = process.env.DB_URL ?? ''
-  console.log('url', url)
   const ormClient = libSqlDrizzle(url, {
     schema,
     casing: 'snake_case',
